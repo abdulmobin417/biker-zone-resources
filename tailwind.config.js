f8f0c75d9f98c4fd902aedcb4d0e5645977a7dd0
AbdulMobin <abdulmobin417@gmail.com>
2023-07-22 04:37:03 +0600

    header section responsive done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      xs: "411px",
+      sm: "540px",
+      smd: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1400px",
+      "3xl": "1920px",
+    },
   },
   plugins: [],
   daisyui: {
